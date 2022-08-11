4181de2649d1863bdc880cabfdaccdc0b51469c4
Henry Miller <mail.henrym+git@gmail.com>
2022-08-11 11:48:00 -0400

    route setup

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,8 +1,24 @@
 import key from '../../apiKey';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { Routes, Route } from 'react-router-dom'
 
 export default function App() {
+  let booksUrl = 'https://www.googleapis.com/books/v1/volumes?q=camus&printType=books&key=' + key
+  let [testData, setTestData] = useState([])
+  useEffect(() => {
+    axios.get(booksUrl)
+      .then(r => setTestData(r.data))
+  }, [])
+
   return (
     <div>
+      <Routes>
+        <Route path='/*' element ={
+          <div>Hello!</div>
+        }
+        />
+      </Routes>
     </div>
   );
 }
