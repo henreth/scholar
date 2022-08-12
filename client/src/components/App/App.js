617ae08607e3875794f1ac295fdbab493f38c107
Henry Miller <mail.henrym+git@gmail.com>
2022-08-12 09:40:41 -0400

    make Home BookContainer BookCard components

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -2,6 +2,7 @@ import key from '../../apiKey';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
+import Home from '../Home/Home';
 
 export default function App() {
   let booksUrl = 'https://www.googleapis.com/books/v1/volumes?q=camus&printType=books&key=' + key
@@ -15,7 +16,7 @@ export default function App() {
     <div>
       <Routes>
         <Route path='/*' element ={
-          <div>Hello!</div>
+          <Home />
         }
         />
       </Routes>
---
+++ client/src/components/BookContainer/BookContainer.js
@@ -0,0 +1,5 @@
+export default function BookContainer(){
+    return (
+        <div>BookContainer</div>
+    )
+}
