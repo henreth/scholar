13835b70f7d767ac83ad99bf2ee528998f91938c
Henry Miller <mail.henrym+git@gmail.com>
2022-08-14 18:11:47 -0400

    add test buttons on home

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,11 +1,28 @@
 import BookContainer from "../BookContainer/BookContainer";
+import axios from 'axios'
 
 export default function Home({testData}){
+
+    const handleLogIn = () => {
+        axios.post('/login',{"username":"test",
+    "password":"12345"})
+        
+    }
+    
+    const handleShowMe = () => {
+        axios.get('/me')
+        .then(r=>console.log(r.data))
+    }
+
+
     return (
         <div>
-            <BookContainer 
+            {/* <BookContainer 
                 testData={testData}
-            />
+            /> */}
+            <button onClick={handleLogIn}>Log In</button>
+            <button onClick={handleShowMe}>Show Me</button>
+
         </div>
     )
 }
