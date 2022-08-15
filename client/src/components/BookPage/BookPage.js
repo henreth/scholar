c54abb2ea8dd8f0304c0d29841bfef7253f359a3
Henry Miller <mail.henrym+git@gmail.com>
2022-08-15 11:58:42 -0400

    start initial book page styling

--- client/src/components/BookPage/BookPage.js
+++ client/src/components/BookPage/BookPage.js
@@ -1,10 +1,30 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
+import key from '../../apiKey'
 
 export default function BookPage(){
     let params = useParams()
+    let id = params.id
+    let [pageData,setPageData] = useState({})
 
-    // "https://www.googleapis.com/books/v1/volumes/jbwI7K00sJ0C"
+
+    useEffect(()=>{
+        let url = "https://www.googleapis.com/books/v1/volumes/" + id + '?&key=' + key
+        axios.get(url)
+        .then(r=>setPageData(r.data))
+    },[])
+
+    if (!pageData.volumeInfo) return false
+
     return (
-        <div>{params.id}</div>
+        <div className="mainContainer">
+            <div className="sidebar">
+                Sidebar
+            </div>
+            <div className="display">
+                Display
+            </div>
+        </div>
     )
 }
