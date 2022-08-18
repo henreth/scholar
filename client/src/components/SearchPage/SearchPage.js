1354e181909c9d4266fcac5c3804b28af00414a7
Henry Miller <mail.henrym+git@gmail.com>
2022-08-18 13:19:37 -0400

    create searchbar on searchpage

--- client/src/components/SearchPage/SearchPage.js
+++ client/src/components/SearchPage/SearchPage.js
@@ -8,7 +8,7 @@ export default function SearchPage({ searchResults, setSearchResults }) {
     let params = useParams()
     let searchTerm = params.searchTerm
     useEffect(() => {
-        document.title='Search Results - ' + searchTerm
+        document.title = 'Search Results - ' + searchTerm
         let booksUrl = 'https://www.googleapis.com/books/v1/volumes?q=' + searchTerm + '&maxResults=40&printType=books&key=' + key
         axios.get(booksUrl)
             .then(r => {
@@ -20,6 +20,20 @@ export default function SearchPage({ searchResults, setSearchResults }) {
 
     return (
         <div>
+            <div className="bigSearchCard">
+                <div className="searchRow">
+                    <h2>Search: </h2>
+                    <form>
+                        <input
+                            type='text'
+                            className="bigSearch"
+                            value={searchTerm}
+                        />
+                    </form>
+                </div>
+                    {/* <hr></hr> */}
+                <div className="filterRow"> Filters: </div>
+            </div>
             <BookContainer
                 books={searchResults}
             />
