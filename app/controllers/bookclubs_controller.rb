f1efe60a7cabce73b839a15a713026d1aa0f4a6e
Henry Miller <mail.henrym+git@gmail.com>
2022-08-19 18:46:58 -0400

    start bookclub controllers

--- app/controllers/bookclubs_controller.rb
+++ app/controllers/bookclubs_controller.rb
@@ -1,2 +1,48 @@
 class BookclubsController < ApplicationController
+
+    def index
+        @bookclubs = Bookclub.all
+        render json: @bookclubs
+    end
+
+    def update
+        bookclub = Bookclub.find_by(params[:id])
+        bookclub.update!(bookclub_params)
+        render json: bookclub
+    end
+
+    def add_user
+
+    end
+
+    def remove_user
+
+    end
+
+    def add_book
+
+    end
+
+    def remove_book
+
+    end
+
+    def create
+        bookclub = Bookclub.create!(bookclub_params)
+        bookclub.save
+        render json: bookclub, status: :created
+    end
+
+    def destroy
+        bookclub = Bookclub.find_by(params[:id])
+        bookclub.destroy
+        render json: bookclub
+    end
+
+    private
+
+    def bookclub_params
+        params.permit(:id,:name,:books)
+    end
+
 end
